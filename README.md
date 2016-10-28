2886ddb6ffb9ed30d10c76a45a5a6adec59a8400
haydenty <htwade51@gmail.com>
2016-10-27 20:51:00 -0500

    init blog stucture and blog1

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ My personal website to share all my projects and passions.
 **Getting Started:**</br>
 npm install</br>
 bower install</br>
-node server</br>
+npm start or node server</br>
 navigate to localhost:3000</br>
--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var express = require('express');
-
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-// Start the server
-app.set('port', 3000);
 app.all('/*', function(req, res, next){
   res.redirect('/');
 });
 
-var server = app.listen(app.get('port'), function() { //helpful for debuggin but not needed from production (I think)
+var server = app.listen(3000, function() {
   console.log('Express server listening on port ' + server.address().port);
 });
 
 app.get('*', function(req, res) {
 	res.sendFile('./public/index.html');
 });
-
-// You can add robots.txt file via express but since you have a redirect all,
-// I added as a file in the public directory, leaving this here for your info
-
-// app.get('/robots.txt', function (req, res) {
-//     res.type('text/plain');
-//     res.send("User-agent: *\nDisallow:");
-// });
